6f558dbf9e7c560c38ab1facd8e0850e4f1b0611
JeanPaulCE <jpcdlalito@hotmail.com>
2021-11-17 20:32:52 -0600

    header

---
+++ header.php
@@ -0,0 +1,37 @@
+<header>
+        <section class="nav-sec ">
+            <nav class="nav-bar inner-grid">
+                <div class="d-logo">
+                    <a href="/" class="logo">Tourist<span>Life</span></a>
+                </div>
+                <div>
+
+                    <label for="menu" class="bars">
+                        <img src="./imgs/svg/bars.svg" alt="menu bar">
+                    </label>
+                </div>
+                <input id="menu" type="checkbox">
+                <div class="end">
+                    <ul class="nav-items">
+                        <li class="nav-item"><a href="/">inicio</a></li>
+
+                        <label for="menu2" class="menu2">
+
+                            <li class="nav-item-cluster">
+                                <p>categoria</p>
+                                <input id="menu2" type="checkbox">
+                                <ul class="cluster">
+                                    <li><a href="/galeria">Galeria</a></li>
+                                    <li><a href="/playa">Playa</a></li>
+                                    <li><a href="/montana">Montaña</a></li>
+                                    <li><a href="/ciudad">Ciudad</a></li>
+                                </ul>
+                            </li>
+                        </label>
+                        <!-- <li class="nav-item"><a href="">acerca de</a></li> -->
+                        <li class="nav-item"><a href="/inicioSesion">iniciar sesion</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+    </header>
