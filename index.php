55c2fac2902b339fc066dc8ea443bfb49402c1df
JeanPaulCE <jpcdlalito@hotmail.com>
2021-11-17 20:35:37 -0600

    footer

--- index.php
+++ index.php
@@ -162,22 +162,9 @@
         </div>
     </section>
 
-    <!--     FOOTER COMPONENT -->
-    <section class="footer-section">
-        <footer class="footer inner-grid">
-            <ul class="footer-elements center">
-                <span class="st-1">
-                    <li class="footer-element"><i class="fab fa-facebook"></i></li>
-                    <li class="footer-element"><i class="fab fa-instagram"></i></li>
-                    <li class="footer-element"><i class="fab fa-twitter"></i></li>
-                    <li class="footer-element"><i class="fab fa-youtube"></i></li>
-                </span>
-                <li class="footer-element"><a href="">Read the FAQ, the Terms & Conditions and the Privacy
-                        Policy</a></li>
-
-            </ul>
-        </footer>
-    </section>
+    <?php
+        include "footer.php";
+    ?>
 
 
 </body>
